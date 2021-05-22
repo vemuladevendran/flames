fdc19a64e80a39bcb3600edbf6bb34b7674ef617
devendran <devendranvemula@gmail.com>
2021-05-22 15:30:05 +0530

    first change

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input'; 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
   ],
   declarations: [FolderPage]
 })
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition, MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,12 +9,110 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
-
-  constructor(private activatedRoute: ActivatedRoute) { }
-
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  flamesForm: FormGroup;
+  flames = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+  ) {
+    this.flamesForm = this.fb.group({
+      nameOne: ['', [Validators.required]],
+      nameTwo: ['', [Validators.required]],
+    })
   }
 
+  ngOnInit() {
+  }
+
+  private getChars(name: string): string[] {
+    return name.toLowerCase().replace(/ /gi, '').split('')
+  }
+
+  private calculateFlames = (nameOne, nameTwo) => {
+    const firstName = this.getChars(nameOne);
+    const secondName = this.getChars(nameTwo);
+    const charMap: any = {};
+    [...firstName, ...secondName].forEach((k) => {
+      charMap[k] = (charMap[k] || 0) + 1;
+    });
+
+    Object.entries(charMap).forEach(([k, v]: [string, number]) => {
+      if ((v > 2) && (v % 2 !== 0)) {
+        charMap[k] = 1;
+      }
+    });
+
+
+    const remaining = Object.keys(charMap).filter(k => charMap[k] === 1).length;
+    this.findFlames(remaining);
+  }
+
+  private findFlames(remaining) {
+    let string = 'flames';
+    let number = remaining;
+    let process = new Array();
+
+    while (string.length >= 2) {
+      var j = 0;
+      process = [];
+      let position = number % string.length;
+      let i = 0
+
+      if (position != 0) {
+        for (i = position + 1; i <= string.length; i++) {
+          process[j] = string.charAt(i - 1);
+          j++;
+        }
+
+        for (i = 0; i <= position - 2; i++) {
+          process[j] = string.charAt(i);
+          j++;
+        }
+        string = process.toString().replace(/,/gi, "");
+      } else {
+        string = string.slice(0, -1);
+      }
+      this.displayFlames(string);
+    }
+
+  }
+
+  private displayFlames(string) {
+    const required = {
+      f: 'FRIENDS',
+      l: 'LOVER',
+      a: 'AFFECTION',
+      m: 'MARRIAGE',
+      e: 'ENEMY',
+      s: 'SISTER'
+    }
+    this.flames = required[string];
+  }
+
+
+  openSnackBar() {
+    this.snackBar.open(this.flames , 'Close', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
+
+
+  async handleFlames(): Promise<any> {
+    try {
+      const nameOne = this.flamesForm.get('nameOne').value;
+      const nameTwo = this.flamesForm.get('nameTwo').value;
+      if (nameOne != nameTwo) {
+        this.calculateFlames(nameOne, nameTwo);
+        this.openSnackBar();
+      }
+
+    } catch (error) {
+      console.error('both name are same ');
+    }
+  }
 }
+
