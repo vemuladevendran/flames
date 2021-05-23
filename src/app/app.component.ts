ae8f52fbd8d2996dfd3d7a64c7b44cc8495efc8a
devendran <devendranvemula@gmail.com>
2021-05-23 10:17:38 +0530

    share option include

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() { }
+
+ 
+  async shareNews(): Promise<any> {
+    const shareData = {
+      title: 'Flames App',
+      text: 'Have Fun',
+      url: '',
+    };
+
+    try {
+      await navigator.share(shareData);
+      console.log('App shared successfully');
+    } catch (error) {
+      console.error('Share failed:', error.message);
+    }
+  }
 }
